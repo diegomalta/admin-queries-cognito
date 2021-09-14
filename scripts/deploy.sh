dce49d9513e665105715c12ef1468b82577b3fdb
diegomalta <diego_malta@hotmail.com>
2021-09-13 21:22:43 -0500

    add cloudformation deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -ex
 
+APP_NAME=admin-queries-cognito
+
 npm install typescript
 npm install & npm run build-prod
+aws cloudformation deploy --template-file template.yml --stack-name ${APP_NAME} --parameter-overrides name=${APP_NAME}
