32252f116634b9b6c8333224f9930d7c6e111ec8
diegomalta <diego_malta@hotmail.com>
2021-11-23 22:09:56 -0600

    CAPABILITY_IAM

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ APP_NAME=admin-queries-cognito
 npm install typescript
 npm install & npm run build-prod
 aws cloudformation package --template-file template.yml --output-template-file api/transformed.yml --s3-bucket ${AWS_ACCOUNT_ID}-code-storage
-aws cloudformation deploy --template-file api/transformed.yml --stack-name ${APP_NAME} --parameter-overrides name=${APP_NAME}
+aws cloudformation deploy --template-file api/transformed.yml --stack-name ${APP_NAME} --parameter-overrides name=${APP_NAME} --capabilities CAPABILITY_IAM
