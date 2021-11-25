6e63e86b14ad5b7b1a1f8888ff60111129931fb6
diegomalta <diego_malta@hotmail.com>
2021-11-25 13:55:10 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,5 @@ APP_NAME=admin-queries-cognito
 npm install typescript
 npm install & npm run build-prod
 aws cloudformation package --template-file template.yml --output-template-file api/transformed.yml --s3-bucket ${AWS_ACCOUNT_ID}-code-storage
-aws cloudformation deploy --template-file api/transformed.yml --stack-name ${APP_NAME} --parameter-overrides name=${APP_NAME} --capabilities CAPABILITY_IAM
+aws s3 cp api/openapi.yml s3://${AWS_ACCOUNT_ID}-code-storage
+aws cloudformation deploy --template-file api/transformed.yml --stack-name ${APP_NAME} --parameter-overrides name=${APP_NAME} templateUrl=s3://${AWS_ACCOUNT_ID}-code-storage/openapi.yml --capabilities CAPABILITY_IAM
