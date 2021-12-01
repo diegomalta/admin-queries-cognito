0feee6c3443b1a5fffcc065f2647ca83367fe2f2
diegomalta <diego_malta@hotmail.com>
2021-11-30 22:38:37 -0600

    change index to test in cloud

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import * as router from 'aws-lambda-router';
-import { ProxyIntegrationEvent } from 'aws-lambda-router/lib/proxyIntegration'
 import * as userController from './controllers/Users';
 
 export const handler = router.handler({
@@ -8,8 +7,8 @@ export const handler = router.handler({
         routes: [{
             path: '/users/list',
             method: 'GET',
-            action: (request: ProxyIntegrationEvent<{ text: string }>, context) => {
-                return `You called me with: ${request.body.text}`
+            action: (request, context) => {
+                return `You called me`;
             }
         }]
     }
