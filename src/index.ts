02ed02b4616eb183e62e38f9dcb0f06742f5f96f
diegomalta <diego_malta@hotmail.com>
2021-12-03 23:16:36 -0600

    fixing typo

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export const handler = router.handler({
             path: '/users/list',
             method: 'GET',
             action: (request, context) => {
-                const userInfo = parseToken(request.headers.Autorization.split(" ")[1]);
+                const userInfo = parseToken(request.headers.Authorization.split(" ")[1]);
                 console.log("USERINFO: \n" + JSON.stringify(userInfo, null, 2));
                 return `You called me with `;
             }
