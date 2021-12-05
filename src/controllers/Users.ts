027b5d08d17e5750c702c855caf093aa37d8ac63
diegomalta <diego_malta@hotmail.com>
2021-12-04 22:18:32 -0600

    use users controller

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,25 +1,30 @@
-import { ProxyIntegrationEvent } from 'aws-lambda-router/lib/proxyIntegration'
-
-export const getUserList = (request: ProxyIntegrationEvent<{ text: string }>, context) => {
+import {
+    ProxyIntegrationEvent,
+    ProxyIntegrationRoute
+  } from "aws-lambda-router/lib/proxyIntegration";
+  import { parseToken } from '../common/parseToken';
+  
+  
+  const checkGroup = (request: ProxyIntegrationEvent<unknown>) => {
+    const userInfo = parseToken(request.headers.Autorization.split(" ")[1]);
+    console.log("USERINFO: \n" + JSON.stringify(userInfo, null, 2));
+  };
+  
+  export const getUserList: ProxyIntegrationRoute["action"] = async (
+    request,
+    context
+  ) => {
+    checkGroup(request);
+  
     return {
-        statusCode: 200,
-        body: "message"
-    }
-}
-
-export const getUserTest = (request: ProxyIntegrationEvent<{ text: string }>, context) => {
-    return {
-        statusCode: 200,
-        body: "message test"
-    }
-}
-
-export const routes = [
-    {
-        path: '/users/list',
-        method: 'GET',
-        action: (request: ProxyIntegrationEvent<{ text: string }>, context) => {
-            return `You called me with: ${request.body.text}`
-        }
-    }
-]
+      statusCode: 200,
+      body: `You called user list`,
+    };
+  };
+  
+  export const routes = [{
+      path: '/users/list',
+      method: 'GET',
+      action: getUserList
+  }];
+  
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,9 @@
 import * as router from 'aws-lambda-router';
 import * as userController from './controllers/Users';
-import { parseToken } from './common/parseToken';
 
 export const handler = router.handler({
     proxyIntegration: {
         cors: true,
-        routes: [{
-            path: '/users/list',
-            method: 'GET',
-            action: (request, context) => {
-                try {
-                    console.log("REQUEST: \n" + JSON.stringify(request, null, 2));
-                    const tokenInfo = parseToken(request.headers.Authorization.split(' ')[1]);
-                    console.log("TOKEN: \n" + JSON.stringify(tokenInfo, null, 2));
-                } catch (e) {
-                    console.log(e);
-                }
-
-                return `You called me with `;
-            }
-        }]
+        routes: userController.routes
     }
 })
