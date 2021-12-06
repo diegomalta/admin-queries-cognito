10f8d3583a0204e2415b4c8d69e362d3e9da8613
diegomalta <diego_malta@hotmail.com>
2021-12-05 20:28:01 -0600

    return user list

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -1,44 +1,52 @@
 import {
-    ProxyIntegrationEvent,
-    ProxyIntegrationRoute
-  } from "aws-lambda-router/lib/proxyIntegration";
-  import { parseToken } from '../common/parseToken';
-  import { CognitoIdentityServiceProvider } from 'aws-sdk';
-  
-  const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
-  const userPoolId = process.env.USERPOOL;
+  ProxyIntegrationEvent,
+  ProxyIntegrationRoute,
+} from "aws-lambda-router/lib/proxyIntegration";
+import { parseToken } from "../common/parseToken";
+import { CognitoIdentityServiceProvider } from "aws-sdk";
 
-  const checkGroup = (request: ProxyIntegrationEvent<unknown>) => {
-    const userInfo = parseToken(request.headers.Authorization.split(" ")[1]);
-    console.log("USERINFO: \n" + JSON.stringify(userInfo, null, 2));
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+const userPoolId = process.env.USERPOOL;
+
+const checkGroup = (request: ProxyIntegrationEvent<unknown>) => {
+  const userInfo = parseToken(request.headers.Authorization.split(" ")[1]);
+  console.log("USERINFO: \n" + JSON.stringify(userInfo, null, 2));
+};
+
+export const getUserList: ProxyIntegrationRoute["action"] = async (
+  request,
+  context
+) => {
+  checkGroup(request);
+
+  const params = {
+    UserPoolId: userPoolId,
   };
-  
-  export const getUserList: ProxyIntegrationRoute["action"] = async (
-    request,
-    context
-  ) => {
-    checkGroup(request);
 
-    const params = {
-        UserPoolId: userPoolId
-    };
+  try {
+    const result = await cognitoIdentityServiceProvider
+      .listUsers(params)
+      .promise();
+    console.log(JSON.stringify(result, null, 2));
 
-    try {
-        const result = await cognitoIdentityServiceProvider.listUsers(params).promise();
-        console.log(JSON.stringify(result, null, 2));
-    } catch (e) {
-        console.log(e);
-    }
-    
     return {
       statusCode: 200,
-      body: `You called user list`,
+      body: JSON.stringify(result),
     };
-  };
-  
-  export const routes = [{
-      path: '/users/list',
-      method: 'GET',
-      action: getUserList
-  }];
-  
+  } catch (e) {
+    console.log(e);
+
+    return {
+      statusCode: 500,
+      body: "error getting cognito users",
+    };
+  }
+};
+
+export const routes = [
+  {
+    path: "/users/list",
+    method: "GET",
+    action: getUserList,
+  },
+];
